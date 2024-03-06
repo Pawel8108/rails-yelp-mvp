3e5fa9e5e31a20ad009f15987ef4850d6d57b291
Pawel Nowak <paqlnis81@gmail.com>
2024-03-06 07:08:52 +0100

    rails-yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+5.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["Chinese", "Italian", "Japanese", "French", "Thai"].sample
+  )
+end
+
